c95e9272e599614461228b4399fc531e1b2900ec
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2023-01-21 11:58:49 -0300

    feat: create player

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,18 +1,6 @@
 defmodule ExMon do
-  @moduledoc """
-  Documentation for `ExMon`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExMon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  alias ExMon.Player
+  def create_player(move_average, move_heal, move_special, name) do
+    Player.create(move_average, move_heal, move_special, name)
   end
 end
---
+++ lib/player.ex
@@ -0,0 +1,17 @@
+defmodule ExMon.Player do
+  @hit_points 100
+  @required_keys [:hit_points, :move_average, :move_heal, :move_special, :name]
+
+  @enforce_keys @required_keys
+  defstruct @required_keys
+
+  def create(move_average, move_heal, move_special, name) do
+    %ExMon.Player{
+      hit_points: @hit_points,
+      move_average: move_average,
+      move_heal: move_heal,
+      move_special: move_special,
+      name: name,
+    }
+  end
+end
