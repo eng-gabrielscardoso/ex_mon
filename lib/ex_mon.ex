c4e3a04a0560a2adc569ad8ff9d5e55817941301
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2023-01-21 16:11:51 -0300

    feat: create game agent to control global state

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,18 @@
 defmodule ExMon do
+  alias ExMon.Game
   alias ExMon.Player
+
+  @enemies_names ["Robotinik", "Super-globalist", "Iphone Socialist", "Marx Lover"]
+
   def create_player(move_average, move_heal, move_special, name) do
     Player.create(move_average, move_heal, move_special, name)
+  end
+
+  def start_game(player) do
+    @enemies_names
+    |> Enum.shuffle
+    |> hd
+    |> create_player(:punch, :cure, :super_punch)
+    |> Game.start(player)
   end
 end
